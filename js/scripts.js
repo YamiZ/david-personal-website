884fc58a3a402f6ae58a75c2811ed60d04f33164
David Moran <dave.moran118@gmail.com>
2016-06-28 23:41:33 -0400

    Final education content

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@ $(".button-collapse").sideNav();
 
 // Slide and fade up for welcome
 $(document).ready(function(){
-	$("#welcome").delay(500).fadeIn(1000);
+	$("#welcome").fadeIn(1000);
 });
 
 //ScrollFire
 var options = [
-    {selector: '#education', offset: 0, callback: function() {
-      Materialize.showStaggeredList("#staggered-test");
+    {selector: '#education', offset: 200, callback: function() {
+      Materialize.showStaggeredList("#education ul");
     } },
 ];
 Materialize.scrollFire(options);
