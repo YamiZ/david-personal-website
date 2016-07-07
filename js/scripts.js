ed3d9e7c8ae375b1474030bdf3a06a57be7cc29c
DaveM118 <dmoran@intertechmedia.com>
2016-07-07 13:16:15 -0400

    New Index file layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,6 @@
 // Initialize collapse button
 $(".button-collapse").sideNav();
 
-// Slide and fade up for welcome
-$(document).ready(function(){
-	$("body").fadeIn(1500);
-});
-
 //Enable Push Pin
 $(document).ready(function(){
     $('#toc .table-of-contents').pushpin({ top: $('#toc').offset().top });
